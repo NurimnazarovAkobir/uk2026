ff0ff22bd1f5293f9c313307601f20a5f1f99e1a
NurimnazarovAkobir <nurimnazarovakobir202@gmail.com>
2026-04-15 02:18:37 +0500

    Add UK flag favicon in rounded shape

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,10 @@ import '../styles/globals.css';
 
 export const metadata = {
   title: 'UK Seasonal Work 2026',
-  description: 'Ariza qoldirish formasi'
+  description: 'Ariza qoldirish formasi',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
